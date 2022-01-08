3b774f3718b66febcf123fa43923b5af132f558d
Sukalyan <sukalyandash1920@gmail.com>
2022-01-08 17:28:25 +0530

    Employee Wage Programs

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace EmployeeWageProblem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //UC1_EmployeePresent.GetAttendance();
+            //UC2_DailyEmployeeWage.EmpDailyWage();
+            //UC3_PartTimeEmpWage.PartTimeEmpWage();
+            //UC4_EmpWageUsingSwitch.EmpWageUsingSwitch();
+            //UC5_CalculateWagesFor20DaysInMonth.CalculateWages();
+            UC6_CalculateWageTill100HrsOr20IsReached.CalculateWageTill100HrsOr20IsReached();
+        }
+    }
+}
